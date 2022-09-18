ef1f72feb1a8215e6610f16a736e57f3adc6737a
Jeffery Patterson <jpatterson933@ucla.edu>
2022-09-18 00:30:09 -0700

    first assignemnt for chapter 2

---
+++ 2_chapter.js
@@ -0,0 +1,20 @@
+/*
+    The goal of this exercise was to write a loop that printed out the folowing using console.log();
+#
+##
+###
+####
+#####
+######
+#######
+*/
+
+let hash = "";
+
+for (i = 0; i < 7; i++) {
+    let pound = "#";
+    hash = hash + pound;
+
+    console.log(hash);
+}
+
