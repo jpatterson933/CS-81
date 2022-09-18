869dc082266e008325af16344032a7833dd0e806
Jeffery Patterson <jpatterson933@ucla.edu>
2022-09-18 16:10:11 -0700

    second activity complete in chapter 2

--- 2_chapter.js
+++ 2_chapter.js
@@ -16,5 +16,26 @@ for (i = 0; i < 7; i++) {
     hash = hash + pound;
 
     console.log(hash);
+};
+
+/* FIZZ BUZZ Write a program that uses console.log to print all numbers from 1 to 100 except
+numbers divisible by 3 print "fizz" and numbers divisible by 5 and not 3 print Buzz THEN
+numbers divisible by both 3 and 5 print FIZZBUZZ */
+
+for (i = 0; i <= 100; i++) {
+    console.log(i)
+
+    switch (0) {
+        case ((i % 3)):
+            console.log("Fizz");
+            break;
+        case ((i % 5)):
+            console.log("Buzz");
+            break;
+    }
+
+    if (((i % 3) + (i % 5)) == 0) {
+        console.log("FizzBuzz")
+    }
 }
 
