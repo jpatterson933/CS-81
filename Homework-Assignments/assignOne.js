488c30fc374c92133a1b0204ee7b285f7afb44e3
Jeffery Patterson <jpatterson933@ucla.edu>
2022-09-22 23:24:41 -0700

    first week of class. adjusted folder and file structure to better suite my goals and tasks while learning and doing work

---
+++ Homework-Assignments/assignOne.js
@@ -0,0 +1,23 @@
+// Problem 1
+/*
+    According to Eloguent Javascript by Marijn Haverbeke, the "Java language was being
+    heavily marketed and was gaining popularity" at the time, and someone thought it would
+    be a good idea to ride the popularity of Java and now we have JavaScript.
+*/
+// Problem 2
+
+let first = "Jeffery";
+let middle = "William";
+let last = "Patterson";
+
+const myName = first + " " + middle + " " + last;
+
+console.log(myName);
+
+// Problem 3
+// Problem 4
+// Problem 5
+// Problem 6
+// Problem 7
+// Problem 8
+
