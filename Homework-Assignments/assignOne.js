9377efa5ab91866864bbeea2b4c089079ba9e4b9
Jeffery Patterson <jpatterson933@ucla.edu>
2022-09-22 23:46:09 -0700

    complete assignment 1 week 1

--- Homework-Assignments/assignOne.js
+++ Homework-Assignments/assignOne.js
@@ -15,9 +15,36 @@ const myName = first + " " + middle + " " + last;
 console.log(myName);
 
 // Problem 3
+if (23 == "23") {
+    console.log("true");
+}
+
+// ANSWER: true
+
 // Problem 4
-// Problem 5
+
+let problemFour = `Infinity is ${5/0}`
+console.log(problemFour)
+
+// ANSWER: "Infinity is Infinity"
+
 // Problem 6
+
+let problemSix = 501 % 100;
+
+console.log(problemSix)
+
+// ANSWER: 1
+
 // Problem 7
+
+let problemSeven = "apples " + (false ? "apple" : (true ? "and" : "peaches")) + " " + ("ba" + "" + 3/"a" + "as");
+
+console.log(problemSeven)
+
+// ANSWER: "apples and baNaNas"
+
 // Problem 8
+
+// ANSWER: Complete with submission of this file.
 
