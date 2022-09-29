b7cfca8db8c58a3c2ce2c366818f34500fb49c4f
Jeffery Patterson <jpatterson933@ucla.edu>
2022-09-29 15:51:34 -0700

    started assignemnt two

---
+++ Homework-Assignments/assignTwo.js
@@ -0,0 +1,10 @@
+/*
+Jeffery W. Patterson
+*/
+
+/* Write a program that uses console.log() to print
+numbers starting at 20 and ending at 0.
+
+For every odd number print "Buzz" instead of the number
+For every even number print "Fizz" insead of the number
+*/
