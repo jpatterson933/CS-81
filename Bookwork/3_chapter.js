9ad62ce5a354a831883d1b51e3ff08d73e87738a
Jeffery Patterson <jpatterson933@ucla.edu>
2022-10-07 12:35:44 -0700

    assignment 1 chapter 3

---
+++ Bookwork/3_chapter.js
@@ -0,0 +1,11 @@
+/*
+    Jeffery W. Patterson
+    Chapter 3 FUNCTIONS
+*/
+
+function returnMin (value1, value2) {
+    console.log(Math.min(value1, value2))
+}
+
+returnMin(23, 45)
+returnMin(28828, 89)
