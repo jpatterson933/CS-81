9b94577f3f6e7785e815471d2efdd47aa708d450
Jeffery Patterson <jpatterson933@ucla.edu>
2022-10-24 23:25:08 -0700

    finished exercise 1 in chapter 6

--- Bookwork/6_chapter.js
+++ Bookwork/6_chapter.js
@@ -3,3 +3,39 @@
     Give the Vec prototype two methods, plus and minus, that take another vector as a parameter and return a new vector that has the sum or difference of the two vectors' (this and the parameters) x and y values.
     Add a getter property length to the prototype that computes the length of the vector--that is, the distance of the point (x,y) from the origin (0,0)
  */
+
+class Vec {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    plus(vector) {
+        let x = this.x + vector.x
+        let y = this.y + vector.y
+        return new Vec(x, y)
+    };
+
+    minus(vector) {
+        let x = this.x - vector.x;
+        let y = this.y - vector.y;
+        return new Vec(x, y);
+    }
+
+    get calculateHypotenuse() {
+        return (Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2)));
+    }
+
+
+}
+
+let cosmo = new Vec(3, 4);
+let drank = new Vec(3, 4);
+let biggie = new Vec(22, 12);
+
+console.log(drank.calculateHypotenuse)
+
+// console.log(cosmo)
+// console.log(cosmo.plus(drank));
+// console.log(cosmo.minus(biggie));
+
